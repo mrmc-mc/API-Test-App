ce72a1d942e28245f98e8ea4c1ce0642c6a242ed
mrmc <messifrd7@gmail.com>
2022-04-17 13:34:30 +0430

    Fix ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*',] # TODO: config('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-THIRD_PARTY_APP
+THIRD_PARTY_APPS = [
+
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
