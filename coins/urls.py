0c807f5553453f16e4aa525013748498be2fb121
mrmc <messifrd7@gmail.com>
2022-04-28 16:00:44 +0430

    Add drf rout config to coins app

--- coins/urls.py
+++ coins/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'trade', views.ArticleViewSet)
 
 app_name = 'coins'
 
 urlpatterns = [
-    # path(),
+    path("", include("router.urls")),
 
 ]
