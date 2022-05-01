c10dd2924a32d449269e2215462f93d8537ea3ea
mrmc <messifrd7@gmail.com>
2022-05-01 10:39:18 +0430

    update permissions.py

--- coins/permissions.py
+++ coins/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 
 
- 
 class TradePermission(permissions.BasePermission):
-    '''
-        permission check for user can trade or not
-    '''
+    """
+    permission check for user can trade or not
+    """
+
     def has_permission(self, request, view):
         return request.user.can_trade
